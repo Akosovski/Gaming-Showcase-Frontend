ac1faedf10a8ecf12a3df62e2d4080ac6545801f
Muhammad Akhta <akhtacaesar@gmail.com>
2023-12-19 23:14:18 +0700

    Intial Next JS Project Creation
    Install Next JS, Tailwind CSS & Setups

---
+++ pages/_document.js
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+
+        
+      </Head>
+      <body className="">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
