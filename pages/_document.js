4f7d137e88a8d84c52608c9ffb1cc86c9ba69d78
Muhammad Akhta <akhtacaesar@gmail.com>
2023-12-20 16:27:44 +0700

    Tailwind not Working

--- pages/_document.js
+++ pages/_document.js
@@ -3,10 +3,6 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head>
-
-        
-      </Head>
       <body className="">
         <Main />
         <NextScript />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     darkMode: 'class',
-    content: ["./src/**/*.{html,js}"],
+    content: [
+      "./app/**/*.{js,ts,jsx,tsx}",
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+    ],
     theme: {
-      extend: {},
+      extend: {
+        fontFamily: {
+          
+        },
+      },
     },
     plugins: [],
   }
